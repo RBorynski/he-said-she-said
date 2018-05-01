8eac52e66fc1d5523103172f235e634c8f6d3a3c
Rafal Borynski <rafalborynski@ool-ad020107.dyn.optonline.net>
2018-05-01 14:22:19 -0400

    installed rails api

--- story_app/src/App.js
+++ story_app/src/App.js
@@ -1,36 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import ReadingPage from "./ReadingPage"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReadingPage from "./ReadingPage";
 // import PrevNextButtons from "./PrevNextButtons"
 
 class App extends Component {
-
-
-
   render() {
     return (
-<Router>
-      <div className="App">
-        <header className="App-header">
-        <h1>  He Said, She Said! </h1>
-        </header>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1> He Said, She Said! </h1>
+          </header>
 
-
-
-
-
-<Route path="/reading" component={ReadingPage}  />
-
-
- </div>
-</Router>
+          <Route path="/reading" component={ReadingPage} />
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
-
-
-
--- story_app/src/ReadingPage/index.js
+++ story_app/src/ReadingPage/index.js
@@ -1,90 +1,79 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import PrevNextButtons from "../PrevNextButtons"
-import './index.css'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrevNextButtons from "../PrevNextButtons";
+import "./index.css";
 
 // console.log(StoriesArray)
-
-
-
-
 
 //create constructor that hardcodes that story
 //page number
 //render the story from the state
 
-
-
-
 class ReadingPage extends Component {
- constructor(props) {
+  constructor(props) {
     super(props);
-    this.state =
-    {page: 0,
+    this.state = {
+      page: 0,
 
-sadStory : [ {quote: "Two friends stood on a street corner.",
-      char: "narrator",
-      sequence:1
-    },
-   {
-    quote: "See you later, Jim",
-    char: "Tim",
-    sequence: 2
-   },
-   {quote: "Bye Tim.See you tomorrow.",
-    char: "Jim",
-    sequence: 3},
-    {
-      quote : "They never met again.",
-      char : "narrator",
-      sequence: 4
-    }
-
-]
-
-    } ;
+      sadStory: [
+        {
+          quote: "Two friends stood on a street corner.",
+          char: "narrator",
+          sequence: 1
+        },
+        {
+          quote: "See you later, Jim",
+          char: "Tim",
+          sequence: 2
+        },
+        {
+          quote: "Bye Tim.See you tomorrow.",
+          char: "Jim",
+          sequence: 3
+        },
+        {
+          quote: "They never met again.",
+          char: "narrator",
+          sequence: 4
+        }
+      ]
+    };
 
     // This binding is necessary to make `this` work in the callback
     this.increasePage = this.increasePage.bind(this);
     this.decreasePage = this.decreasePage.bind(this);
   }
 
-increasePage () {
-    if (this.state.page<(this.state.sadStory.length-1))
-    {this.setState({page: this.state.page + 1});}
-  console.log(this.state.page)
+  increasePage() {
+    if (this.state.page < this.state.sadStory.length - 1) {
+      this.setState({ page: this.state.page + 1 });
+    }
+    console.log(this.state.page);
   }
 
-  decreasePage () {
-    console.log(this.state.page)
-    if (this.state.page>0)
-    {this.setState({page: this.state.page - 1});}
-
+  decreasePage() {
+    console.log(this.state.page);
+    if (this.state.page > 0) {
+      this.setState({ page: this.state.page - 1 });
+    }
   }
   render() {
-
-
-
     return (
-
-<div className="MainView">
-    <div ClassName = "CharStuff">
-            <PrevNextButtons
-             page =  {this.state.page}
-             decreasePage = {this.decreasePage}
-             increasePage = {this.increasePage}
-            />
-            <div className = "CurrentChar">
-        <p> {this.state.sadStory[this.state.page].char}</p>
-            </div>
-     </div>
-     <div className = "QuoteScreen">
-     <p> {this.state.sadStory[this.state.page].quote}</p>
-     </div>
-
-</div>
-
-
+      <div className="MainView">
+        <div ClassName="CharStuff">
+          <PrevNextButtons
+            page={this.state.page}
+            decreasePage={this.decreasePage}
+            increasePage={this.increasePage}
+          />
+          <div className="CurrentChar">
+            <p> {this.state.sadStory[this.state.page].char}</p>
+          </div>
+        </div>
+        <div className="QuoteScreen">
+          <p> {this.state.sadStory[this.state.page].quote}</p>
+        </div>
+      </div>
     );
   }
 }
