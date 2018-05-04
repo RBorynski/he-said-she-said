dd0806c22a8775be0b6b97fa8d43efdd3bc40bfa
Rafal Borynski <rafalborynski@ool-ad020107.dyn.optonline.net>
2018-05-04 09:55:50 -0400

    classic styling

--- story_app/src/PrevNextButtons/index.js
+++ story_app/src/PrevNextButtons/index.js
@@ -1,21 +1,12 @@
-import React, { Component } from "react"
-
-class PrevNextButtons extends  Component {
-
-
+import React, { Component } from "react";
+class PrevNextButtons extends Component {
   render() {
     return (
       <div>
-      <button onClick={this.props.decreasePage}>
-        prev &larr;
-      </button>
-      <button onClick={this.props.increasePage}>
-        next &rarr;
-
-      </button>
-    </div>
+        <button onClick={this.props.decreasePage}>prev &larr;</button>
+        <button onClick={this.props.increasePage}>next &rarr;</button>
+      </div>
     );
   }
-
-};
-export default PrevNextButtons
+}
+export default PrevNextButtons;
